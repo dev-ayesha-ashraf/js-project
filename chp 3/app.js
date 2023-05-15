8b11ca8f39bbeda4abd703d35c2fe9bcbc88fa9e
dev-ayesha-ashraf <128064292+dev-ayesha-ashraf@users.noreply.github.com>
2023-05-15 15:19:59 +0500

    Delete chp 3 directory

--- chp 3/app.js
+++
@@ -1,9 +0,0 @@
-// 1. 
-// var age = "18"
-// alert(age);
-
-// 2.
-var visitCount = parseInt(localStorage.getItem('visitCount')) || 0;
-visitCount++;
-localStorage.setItem('visitCount', visitCount);
-alert("You have visited this site " + visitCount + " times");
