4c7e78b414568f2302985ab83290568866e0fb0f
dev-ayesha-ashraf <128064292+dev-ayesha-ashraf@users.noreply.github.com>
2023-06-05 23:46:04 +0500

    chp 14 to 17 added

---
+++ chp 14 -17/app.js
@@ -0,0 +1,100 @@
+// 1 
+// var studentNames = [];
+
+
+// 2 
+// var  studentNames = [];
+
+
+// 3 
+// var fruits = ["apple", "grapes", "orange"];
+
+// 4 
+// var numbers = [1, 2, 3, 4, 5];
+
+// 5 
+// var booleanArray = [true, false];
+
+// 6 
+// var mixedArray = [1, "apple", true, { name: "ayesha", age: 18 }];
+
+// 7 
+// var qualifications = ["SSC", "\nHSC", "\nBCS", "\nBS", "\nBCOM", "\nMS", "\nM. Phil.", "\nPhD"];
+
+// document.write("<h1>Available Education Qualifications in Pakistan:</h1>");
+// document.write(qualifications);
+
+
+// 8 
+// var studentNames = ["usman", "wasif", "azeem"]
+// var scores = [320 , 230 , 480];
+// var totalMarks = 500;
+
+// var percentage1 = (scores[0] / totalMarks) * 100;
+// var percentage2 = (scores[1] / totalMarks) * 100;
+// var percentage3 = (scores[2] / totalMarks) * 100;
+
+
+// document.write("score of " + studentNames[0] + " is " + percentage1 + "%")
+// document.write( "score of " + studentNames[1] + " is " + percentage2 + "%")
+// document.write( "score of " + studentNames[2] + " is " + percentage3 + "%")
+
+
+
+// 9 
+// var colors = ["Red", "Green", "Blue", "Black"];
+// document.write("<b>Initial Array is</b> " + colors + "<br>");
+// var colorToAddStart = prompt("Enter a color to add to the beginning:");
+// colors.unshift(colorToAddStart);
+// document.write("<b>Array after adding color to the beginning:</b> " + colors + "<br>");
+// var colorToAddEnd = prompt("Enter a color to add to the end:");
+// colors.push(colorToAddEnd);
+// document.write("<b>Array after adding color to the end:</b> " + colors + "<br>");
+// colors.unshift("Purple", "Orange");
+// document.write("<b>Array after adding two colors to the beginning:</b> " + colors + "<br>");
+// colors.shift();
+// document.write("<b>Array after deleting the first color:</b> " + colors + "<br>");
+// colors.pop();
+// document.write("<b>Array after deleting the last color:</b> " + colors + "<br>");
+// var indexToAdd = prompt("Enter the index at which you want to add a color:");
+// var colorToAddIndex = prompt("Enter the color to add:");
+// colors.splice(indexToAdd, 0, colorToAddIndex);
+// document.write("<b>Array after adding color at index " + indexToAdd + ":</b> " + colors + "<br>");
+// var indexToDelete = prompt("Enter the index from where you want to delete color(s):");
+// var numberOfColorsToDelete = prompt("Enter the number of colors to delete:");
+// colors.splice(indexToDelete, numberOfColorsToDelete);
+// document.write("<b>Array after deleting " + numberOfColorsToDelete + " color(s) from index " + indexToDelete + ":</b> " + colors + "<br>");
+
+
+// 10 
+// var scores = [85, 92, 78, 95, 88];
+// console.log("Initial scores: " + scores);
+// scores.sort(function(a, b) {
+//   return a - b;
+// });
+// console.log("Sorted scores (ascending order): " + scores);
+
+
+
+// 11 
+// var cities = ["karachi" , "lahore" , "islamabad" , "quetta", "peshawar"];
+// var selectedCiteies= [cities[2] , cities[3]];
+// document.write("<h1>cities list</h1>")
+// document.write(cities);
+// document.write("<h1>selected cities list</h1>")
+// document.write(cities[2]+ ","+ cities[3])
+
+
+// 12
+// var sentence = ["This", "is", "my", "cat"];
+// var singleString = sentence.join(" ");
+// document.write("<h1>Array:</h1>")
+// document.write(sentence)
+// document.write("<h1>string</h1>")
+// document.write(singleString);
+
+// 13 
+
+// 14 
+
+// 15 
